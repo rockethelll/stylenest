ab70d61c9ea499781be3b2d7ecc6be1ac98f6dd0
rockethelll <rockethell@hotmail.com>
2025-02-21 19:41:08 +0100

    initial commit

---
+++ src/components/signature-greatfrontend.tsx
@@ -0,0 +1,28 @@
+const SignatureGreatfrontend = () => {
+  return (
+    <div
+      className='text-[oklch(56.93% 0 102.07)] fixed text-center bottom-6 left-6 text-[0.6875rem] w-full mx-auto'
+      data-gfe-screenshot-exclude='true'
+    >
+      A challenge by{' '}
+      <a
+        href='https://www.greatfrontend.com/projects?ref=challenges'
+        className='text-black font-bold'
+        target='_blank'
+      >
+        GreatFrontEnd Projects
+      </a>{' '}
+      . Built by
+      <a
+        href='https://www.greatfrontend.com/projects/u/rockethell'
+        className='text-black font-bold'
+        target='_blank'
+      >
+        Rockethell
+      </a>{' '}
+      .
+    </div>
+  );
+};
+
+export default SignatureGreatfrontend;
